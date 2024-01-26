147aee0cad8ec2ea3c9885652ab3f9baaf8fae43
Jaakko Ruhanen <jaakko.ruhanen@outlook.com>
2024-01-26 19:52:59 +0200

    added some padding

--- client/src/layouts/Main.tsx
+++ client/src/layouts/Main.tsx
@@ -1,13 +1,15 @@
 import { Box } from "@mui/material"
 import { ReactNode } from "react"
-import { useIsTiny } from "src/hooks/useResponsive"
+import { useIsMobile, useIsTiny } from "src/hooks/useResponsive"
 
 type Props = {
     children: ReactNode
 }
 
 export default function Main({ children }: Props) {
-    const margin = (useIsTiny()) ? 0 : 4
+    const isMobile = useIsMobile()
+    const isTiny = useIsTiny()
+    const margin = isMobile ? (isTiny ? 1 : 2) : 4
     return (
         <main>
             <Box sx={{m: margin, mt:4}} minHeight={'100vh'}>
